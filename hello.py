e9961a9eebc73f31e46e325dca47205454a88a82
CHARAN <bjaicharanreddy00@gmail.com>
2026-09-24 12:02:50 +0530

    update Python script

--- hello.py
+++ hello.py
@@ -1,3 +1,3 @@
 print("Hello from Github Actions!")
-print("Today I started learning Github Actions.")
-print("Python script executed successfully.")
+print("I am  learning CI/CD with Github Actions.")
+print("My second workflow run is successful!.")
